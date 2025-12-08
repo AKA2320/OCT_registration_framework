458f6ddf0b87c01c0e70e07204e31f3ba82f1787
akash <akash2320@gmail.com>
2025-12-08 10:54:19 -0500

    refactor: update infer_x_translation to return both translations and streamline x_correction functions

--- utils/x_correction_util_funcs.py
+++ utils/x_correction_util_funcs.py
@@ -60,73 +60,83 @@ def check_multiple_warps(stat_img, mov_img, *args):
     return np.argmax(errors)
 
 def compute_transform_for_pair(i, static_img, moving_img, cells_coords, enface_extraction_rows, MODEL_X_TRANSLATION, valid_args):
-    cell_warps = []
     if i not in valid_args:
         return AffineTransform(translation=(0,0))
-    try:
-        if (cells_coords is not None):
-            if MODEL_X_TRANSLATION is not None:
-                for UP_x, DOWN_x in cells_coords:
-                    stat = static_img[UP_x:DOWN_x, :]
-                    temp_manual = moving_img[UP_x:DOWN_x, :]
-                    temp_cell_shift = np.squeeze(infer_x_translation(MODEL_X_TRANSLATION, stat, temp_manual))[0]
-                    inv_temp_cell_shift = np.squeeze(infer_x_translation(MODEL_X_TRANSLATION, temp_manual, stat))[0]
-                    error_cell = abs(temp_cell_shift + inv_temp_cell_shift)
-                    cell_warps.append((error_cell, temp_cell_shift))
-            else:
-                if cells_coords.shape[0]==1:
-                    UP_x, DOWN_x = cells_coords[0,0], cells_coords[0,1]
-                    stat = static_img[ UP_x:DOWN_x, :]
-                    temp_manual = moving_img[ UP_x:DOWN_x, :]
-                else:
-                    stat = static_img[np.r_[tuple(np.r_[start:end] for start, end in cells_coords)],:]
-                    temp_manual = moving_img[np.r_[tuple(np.r_[start:end] for start, end in cells_coords)],:]
-                # MANUAL
-                temp_cell_patch_shift = get_cell_patch_shift(stat,temp_manual)
-                inv_temp_cell_patch_shift = get_cell_patch_shift(temp_manual,stat)
-                error_cell = abs(temp_cell_patch_shift + inv_temp_cell_patch_shift)
-                cell_warps.append((error_cell, temp_cell_patch_shift))
+    if (cells_coords is not None):
+        if MODEL_X_TRANSLATION is not None:
+            cell_warps = x_correction_cell_model(static_img, moving_img, cells_coords, MODEL_X_TRANSLATION)
         else:
-            cell_warps = [(float('inf'), 0.0)]
-    except Exception as e:
+            cell_warps = x_correction_cell_manual(static_img, moving_img, cells_coords)
+    else:
         cell_warps = [(float('inf'), 0.0)]
-    # enface_shape = data[:,0,:].shape[1]
-    enface_wraps = []
     if len(enface_extraction_rows)>0:
-        for enf_idx in range(len(enface_extraction_rows)):
-            try:
-                if MODEL_X_TRANSLATION is not None:
-                    bottom_row = max(0, enface_extraction_rows[enf_idx]-32)
-                    stat = static_img[bottom_row:enface_extraction_rows[enf_idx]+32]
-                    temp_manual = moving_img[bottom_row:enface_extraction_rows[enf_idx]+32]
-                    temp_enface_shift = np.squeeze(infer_x_translation(MODEL_X_TRANSLATION, stat, temp_manual))[0]
-                    inv_temp_enface_shift = np.squeeze(infer_x_translation(MODEL_X_TRANSLATION, temp_manual, stat))[0]
-                    error_enface = abs(temp_enface_shift + inv_temp_enface_shift)
-                    enface_wraps.append((error_enface, temp_enface_shift))
-                else:
-                    stat = static_img[ enface_extraction_rows[enf_idx]]
-                    temp_manual = moving_img[ enface_extraction_rows[enf_idx]]
-                    temp_enface_shift = get_line_shift(stat, temp_manual)
-                    inv_temp_enface_shift = get_line_shift(temp_manual, stat)
-                    error_enface = abs(temp_enface_shift + inv_temp_enface_shift)
-                    enface_wraps.append((error_enface, temp_enface_shift))
-            except Exception as e:
-                enface_wraps = [(float('inf'), 0.0)]
+        if MODEL_X_TRANSLATION is not None:
+            enface_wraps = x_correction_enface_model(static_img, moving_img, enface_extraction_rows, MODEL_X_TRANSLATION)
+        else:
+            enface_wraps = x_correction_enface_manual(static_img, moving_img, enface_extraction_rows)
+    else:
+        enface_wraps = [(float('inf'), 0.0)]
     all_warps = [*cell_warps,*enface_wraps]
     all_warps = sorted(all_warps, key=lambda x: x[0])  # Sort by error
     temp_tform_manual = AffineTransform(translation=(all_warps[0][1],0))
     return temp_tform_manual
+
+def x_correction_cell_model(static_img, moving_img, cells_coords, MODEL_X_TRANSLATION):
+    cell_warps = []
+    for UP_x, DOWN_x in cells_coords:
+        stat = static_img[UP_x:DOWN_x, :]
+        temp_manual = moving_img[UP_x:DOWN_x, :]
+        temp_cell_shift, inv_temp_cell_shift = infer_x_translation(MODEL_X_TRANSLATION, stat, temp_manual)
+        error_cell = abs(temp_cell_shift + inv_temp_cell_shift)
+        cell_warps.append((error_cell, temp_cell_shift))
+    return cell_warps
+
+def x_correction_cell_manual(static_img, moving_img, cells_coords):
+    if cells_coords.shape[0]==1:
+        UP_x, DOWN_x = cells_coords[0,0], cells_coords[0,1]
+        stat = static_img[ UP_x:DOWN_x, :]
+        temp_manual = moving_img[ UP_x:DOWN_x, :]
+    else:
+        stat = static_img[np.r_[tuple(np.r_[start:end] for start, end in cells_coords)],:]
+        temp_manual = moving_img[np.r_[tuple(np.r_[start:end] for start, end in cells_coords)],:]
+    # MANUAL
+    temp_cell_patch_shift = get_cell_patch_shift(stat,temp_manual)
+    inv_temp_cell_patch_shift = get_cell_patch_shift(temp_manual,stat)
+    error_cell = abs(temp_cell_patch_shift + inv_temp_cell_patch_shift)
+    cell_warps = [(error_cell, temp_cell_patch_shift)]
+    return cell_warps
+
+def x_correction_enface_model(static_img, moving_img, enface_extraction_rows, MODEL_X_TRANSLATION):
+    enface_wraps = []
+    for enf_idx in range(len(enface_extraction_rows)):
+        bottom_row = max(0, enface_extraction_rows[enf_idx]-32)
+        stat = static_img[bottom_row:enface_extraction_rows[enf_idx]+32]
+        temp_manual = moving_img[bottom_row:enface_extraction_rows[enf_idx]+32]
+        temp_enface_shift, inv_temp_enface_shift = infer_x_translation(MODEL_X_TRANSLATION, stat, temp_manual)
+        error_enface = abs(temp_enface_shift + inv_temp_enface_shift)
+        enface_wraps.append((error_enface, temp_enface_shift))
+    return enface_wraps
+
+def x_correction_enface_manual(static_img, moving_img, enface_extraction_rows):
+    enface_wraps = []
+    for enf_idx in range(len(enface_extraction_rows)):
+        stat = static_img[ enface_extraction_rows[enf_idx]]
+        temp_manual = moving_img[ enface_extraction_rows[enf_idx]]
+        temp_enface_shift = get_line_shift(stat, temp_manual)
+        inv_temp_enface_shift = get_line_shift(temp_manual, stat)
+        error_enface = abs(temp_enface_shift + inv_temp_enface_shift)
+        enface_wraps.append((error_enface, temp_enface_shift))
+    return enface_wraps
 
 def x_motion_correction(data, cells_coords, valid_args, enface_extraction_rows, disable_tqdm, scan_num, MODEL_X_TRANSLATION):
     transforms_all = np.tile(np.eye(3),(data.shape[0],1,1))
     indices = [i for i in range(0, data.shape[0]-1, 2)]
     static_imgs = data[indices]
     moving_imgs = data[np.array(indices) + 1]
-    partial_func = partial(compute_transform_for_pair, cells_coords=cells_coords,
-                           enface_extraction_rows=enface_extraction_rows, MODEL_X_TRANSLATION=MODEL_X_TRANSLATION,
-                           valid_args = valid_args)
-    with ThreadPoolExecutor(max_workers = None) as executor:
-        results = list(executor.map(partial_func, indices, static_imgs, moving_imgs))
+    
+    with ThreadPoolExecutor() as executor:
+        compute_fn = partial(compute_transform_for_pair, cells_coords=cells_coords, enface_extraction_rows=enface_extraction_rows, MODEL_X_TRANSLATION=MODEL_X_TRANSLATION, valid_args=valid_args)
+        results = list(executor.map(compute_fn, indices, static_imgs, moving_imgs))
     for i, result in zip(indices, results):
         transforms_all[i+1] = np.dot(transforms_all[i+1], result)
     return transforms_all
